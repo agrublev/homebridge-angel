50d9ceec8fc99cd5e86874f897208e6418e11d84
agrublev <agrublev@gmail.com>
2017-07-26 20:22:52 -0700

    frst

--- index.js
+++ index.js
@@ -64,23 +64,23 @@ function AngelPlatform(log, config) {
 
 function readX10config() {
     var fs = require('fs');
-    var x10confObject = {};
-
-    var x10confData = fs.readFileSync(x10conf);
-    var pattern = new RegExp('\nalias.*', 'ig');
-
-    // ALIAS Front_Porch A1 StdLM
-
-    var match = [];
-    while ((match = pattern.exec(x10confData)) != null) {
-        var line = match[0].split(/[ \t]+/);
-        x10confObject[line[1]] = {
-            'name': line[1].replace(/_/g, ' '),
-            'housecode': line[2],
-            'module': line[3]
-        };
-    }
-    return x10confObject;
+    // var x10confObject = {};
+    //
+    // var x10confData = fs.readFileSync(x10conf);
+    // var pattern = new RegExp('\nalias.*', 'ig');
+    //
+    // // ALIAS Front_Porch A1 StdLM
+    //
+    // var match = [];
+    // while ((match = pattern.exec(x10confData)) != null) {
+    //     var line = match[0].split(/[ \t]+/);
+    //     x10confObject[line[1]] = {
+    //         'name': line[1].replace(/_/g, ' '),
+    //         'housecode': line[2],
+    //         'module': line[3]
+    //     };
+    // }
+    return {};// x10confObject;
 }
 
 function enableFireCracker() {
